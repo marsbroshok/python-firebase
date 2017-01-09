60efe56433fb8cb5cf62904764339323257036ef
Alexander Usoltsev <marsbroshok@gmail.com>
2017-01-09 17:44:04 +0100

    Update __init__.py

--- firebase/__init__.py
+++ firebase/__init__.py
@@ -1,37 +1,38 @@
 import requests
-import urlparse #for urlparse and urljoin
-import os #for os.path.dirname
-import json #for dumps
-
+import urlparse  # for urlparse and urljoin
+import os  # for os.path.dirname
+import json  # for dumps
 
 
 class Firebase():
-    ROOT_URL = '' #no trailing slash
+    ROOT_URL = ''  # no trailing slash
+    auth_token = None
 
     def __init__(self, root_url, auth_token=None):
         self.ROOT_URL = root_url.rstrip('/')
         self.auth_token = auth_token
 
-    #These methods are intended to mimic Firebase API calls.
+    # These methods are intended to mimic Firebase API calls.
 
     def child(self, path):
         root_url = '%s/' % self.ROOT_URL
         url = urlparse.urljoin(root_url, path.lstrip('/'))
-        return Firebase(url)
+        return Firebase(url, auth_token=self.auth_token)
 
     def parent(self):
         url = os.path.dirname(self.ROOT_URL)
-        #If url is the root of your Firebase, return None
+        # If url is the root of your Firebase, return None
         up = urlparse.urlparse(url)
         if up.path == '':
-            return None #maybe throw exception here?
-        return Firebase(url)
+            return None  # maybe throw exception here?
+        return Firebase(url, auth_token=self.auth_token)
 
     def name(self):
         return os.path.basename(self.ROOT_URL)
 
     def toString(self):
         return self.__str__()
+
     def __str__(self):
         return self.ROOT_URL
 
@@ -47,33 +48,31 @@ class Firebase():
     def remove(self):
         return self.delete()
 
-    
-    #These mirror REST API functionality
+    # These mirror REST API functionality
 
     def put(self, data):
-        return self.__request('put', data = data)
+        return self.__request('put', data=data)
 
     def patch(self, data):
-        return self.__request('patch', data = data)
+        return self.__request('patch', data=data)
 
     def get(self):
         return self.__request('get')
 
-    #POST differs from PUT in that it is equivalent to doing a 'push()' in
-    #Firebase where a new child location with unique name is generated and
-    #returned
+    # POST differs from PUT in that it is equivalent to doing a 'push()' in
+    # Firebase where a new child location with unique name is generated and
+    # returned
     def post(self, data):
-        return self.__request('post', data = data)
+        return self.__request('post', data=data)
 
     def delete(self):
         return self.__request('delete')
 
-
-    #Private
+    # Private
 
     def __request(self, method, **kwargs):
-        #Firebase API does not accept form-encoded PUT/POST data. It needs to
-        #be JSON encoded.
+        # Firebase API does not accept form-encoded PUT/POST data. It needs to
+        # be JSON encoded.
         if 'data' in kwargs:
             kwargs['data'] = json.dumps(kwargs['data'])
 
@@ -82,13 +81,12 @@ class Firebase():
             if 'params' in kwargs:
                 params = kwargs['params']
                 del kwargs['params']
-            params.update({'auth': self.auth_token})
+            params.update({'access_token': self.auth_token})
 
         r = requests.request(method, self.__url(), params=params, **kwargs)
-        r.raise_for_status() #throw exception if error
+        r.raise_for_status()  # throw exception if error
         return r.json()
 
-
     def __url(self):
-        #We append .json to end of ROOT_URL for REST API.
+        # We append .json to end of ROOT_URL for REST API.
         return '%s.json' % self.ROOT_URL
